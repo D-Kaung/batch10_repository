a3c2acea6f67ef19d1fcb8c1fd792492db8f49c0
D-Kaung <yatorosama7@gmail.com>
2025-03-03 00:34:19 +0630

    Add files via upload

---
+++ createuser/src/main/java/com/example/demo/createuser/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.example.demo.createuser.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.createuser.model.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+    User findByName(String name);
+
+    Optional<User> findByUsername(String username);
+
+
+    User findUserByUsername(String username);
+}
