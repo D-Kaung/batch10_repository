d44e4af9b87cf3d47e84c0c3daff98ab9aa1e51f
Aung Kaung Htet <yatorosama7@gmail.com>
2025-07-07 14:16:45 +0630

    Delete Ev directory

--- Ev/src/main/java/com/steve/ev/Repository/TransactionRepo.java
+++
@@ -1,7 +0,0 @@
-package com.steve.ev.Repository;
-
-import com.steve.ev.Entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepo extends JpaRepository<Transaction, Long> {
-}
